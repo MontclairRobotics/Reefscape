9fcf19497cc698ef0bedcc2bee8f285727d0af17
cbgpenguin <cbaneygiampoala@gmail.com>
2025-01-07 00:02:03 -0500

    Reply
    reply to Rafael code

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -3,5 +3,6 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    //is git working   
+    //is git working 
+    //Yup!!!!! It seems to be!
 }
