b63e5252ece10d6639c3701375d8ec93abb518bb
cbgpenguin <cbaneygiampoala@gmail.com>
2025-01-07 11:33:30 -0500

    Krakens! + Legs.java

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -1,8 +1,16 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-    //is git working 
-    //Yup!!!!! It seems to be!
+    
+    TalonFX driveFrontLeft = new TalonFX(-1);
+    TalonFX turnFrontLeft = new TalonFX(-1);
+    TalonFX driveFrontRight = new TalonFX(-1);
+    TalonFX turnFrontRight = new TalonFX(-1);
+    TalonFX driveBackLeft = new TalonFX(-1);
+    TalonFX turnBackLeft = new TalonFX(-1);
+    TalonFX driveBackRight = new TalonFX(-1);
+    TalonFX turnBackRIght = new TalonFX(-1);
 }
