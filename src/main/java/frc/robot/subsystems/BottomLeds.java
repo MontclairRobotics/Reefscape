8143dafb79a52321782747a516df38fb23c92b6c
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-01-14 16:56:47 -0500

    Started bottom led subsystem

---
+++ src/main/java/frc/robot/subsystems/BottomLeds.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+
+public class BottomLeds {
+
+    public static final int PORT = -1;
+    public static final int LENGTH = 20;
+    AddressableLED led;
+    AddressableLEDBuffer ledBuffer;
+
+
+    public BottomLeds() {
+        led = new AddressableLED(PORT);
+        ledBuffer = new AddressableLEDBuffer(LENGTH);
+        led.setData(ledBuffer);
+        led.start();
+
+    }
+
+    public void playPattern(LEDPattern pattern){
+        pattern.applyTo(ledBuffer);
+        led.setData(ledBuffer);
+    }
+}
