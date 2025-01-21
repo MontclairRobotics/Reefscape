8340ff7006dfe284fde23af1f9d316f84b2a1b84
Jame <jctee02@gmail.com>
2025-01-21 10:53:55 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/frc/robot/leds/BottomLEDs.java
+++ src/main/java/frc/robot/leds/BottomLEDs.java
@@ -1,76 +1,62 @@
 package frc.robot.leds;
-
-import java.util.Map;
-
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
-import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
-import frc.robot.subsystems.Elevator;
 
-public class BottomLEDs extends SubsystemBase {
-    public static final int PORT = 9;
-    public static final int LENGTH = 100;
-    private static final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
-    private static final Distance kLedSpacing = Units.Meters.of(1 / 100.0);
-    // TODO make private?
-    public static final LEDPattern m_scrollingRainbow = m_rainbow.scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1),
-            kLedSpacing);
+public class BottomLeds extends SubsystemBase{
+    
+    public static final int kPORT = -1;
+    public static final int kLENGTH = -1;
+      
+  private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
+  private final AddressableLEDBuffer m_buffer;
+  private static final Distance kLedSpacing = Units.Meters.of(1 / 120.0);
+  
+  private final LEDPattern m_scrollingRainbow =
+      m_rainbow.scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1), kLedSpacing);
     AddressableLED led;
     AddressableLEDBuffer ledBuffer;
 
-    public BottomLEDs() {
-        led = new AddressableLED(PORT);
-        led.setLength(LENGTH);
-        ledBuffer = new AddressableLEDBuffer(LENGTH);
+    public BottomLeds() {
+
+        led = new AddressableLED(kPORT);
+        ledBuffer = new AddressableLEDBuffer(kLENGTH);
+        led.setData(ledBuffer);
         led.start();
 
-    }
 
-    // TODO this crashes a simulator. Is it needed?
-    // AddressableLEDBufferView m_left = ledBuffer.createView(0, 59);
+    }
+    AddressableLEDBufferView m_left = ledBuffer.createView(0, 59);
 
     // The section of the strip on the right side of the robot.
     // This section spans LEDs from index 60 through index 119, inclusive.
     // This view is reversed to cancel out the serpentine arrangement of the
     // physical LED strip on the robot.
-    // AddressableLEDBufferView m_right = ledBuffer.createView(60, 119).reversed();
-
-    public void rainbowProgress() {
-        final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
-        final LEDPattern progress = LEDPattern
-                .progressMaskLayer(() -> RobotContainer.elevator.getHeight() / Elevator.ELEVATOR_MAX_HEIGHT);
-        LEDPattern pattern = m_rainbow.mask(progress);
-
+    AddressableLEDBufferView m_right = ledBuffer.createView(60, 119).reversed();
+    
+    public void playPattern(LEDPattern pattern){
         pattern.applyTo(ledBuffer);
         led.setData(ledBuffer);
 
-        // for later, trying to make the rainbow scroll ->
-        // Map<Number, Color> maskSteps = Map.of(0, Color.kWhite, 0.5, Color.kBlack);
-        // LEDPattern mask =
-        // LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(0.25));
+    final LEDPattern m_rainbow = LEDPattern.rainbow(255, 128);
 
+    final Distance kLedSpacing = Units.Meters.of(1 / 120.0);
+
+    final LEDPattern m_scrollingRainbow =
+        m_rainbow.scrollAtAbsoluteSpeed(Units.MetersPerSecond.of(1), kLedSpacing);
     }
-
-    public void progressBar() {
-        // LEDPattern progess = LEDPattern.progressMaskLayer(() -> Elevator.getHeight()
-        // / Elevator.getMaxHeight());;
+    public Command runPattern(LEDPattern pattern){
+        return run(() -> pattern.apply(m_buffer));
     }
-
-    @Override
-    public void periodic() {
+    public void robotPeriodic() {
+        // Update the buffer with the rainbow animation
+        m_scrollingRainbow.applyTo(ledBuffer);
+        // Set the LEDs
         led.setData(ledBuffer);
-    }
-
-    // TODO maybe make this an enum or something, not sure tbh
-    public Command playPatternCommand(LEDPattern pattern) {
-        return Commands.run(() -> pattern.applyTo(ledBuffer), this).ignoringDisable(true);
     }
 }
