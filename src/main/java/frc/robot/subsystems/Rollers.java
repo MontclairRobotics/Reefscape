f9d6c306c7ecd72e2e1e765c00233560344ade78
Montclair Code Team <869c568+cbgpenguin@users.noreply.github.com>
2025-01-28 14:00:41 -0500

    Update Rollers.java

--- src/main/java/frc/robot/subsystems/Rollers.java
+++ src/main/java/frc/robot/subsystems/Rollers.java
@@ -8,6 +8,8 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -18,11 +20,17 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Rollers extends Subsystembase {
-    private rightMotor;
-    private leftMotor;
-public void Rollers (){
-    rightMotor = new Sparkmax (-1, MotorType.kBrushless);
-    leftMotor = new Sparkmax (-1, MotorType.kBrushless);
+public class Rollers extends SubsystemBase {
+    private SparkMax rightMotor;
+    private SparkMax leftMotor;
+
+    public Rollers() {
+    rightMotor = new SparkMax(0, null);
+    leftMotor = new SparkMax(0, null);
 }
+
+    public void setIntakeSpeed () {
+    rightMotor.set (1);
+    leftMotor.set (1);
+    }   
 }
