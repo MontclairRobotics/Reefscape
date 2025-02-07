a1a08c06040b6dbb130bf31a76a712c33f5b8437
sub0dev <125705137+Mr-Pyro@users.noreply.github.com>
2025-02-07 15:00:31 -0500

    Update Wrist.java

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -8,8 +8,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.subsystems.Rollers;
+
 public class Wrist extends SubsystemBase {
     private SparkMax wristMotor;
+    public static Rollers rollers = new Rollers();
     public double smallWristAngle;
     public double largeWristAngle;
     Encoder wristEncoder = (Encoder) wristMotor.getEncoder();
@@ -24,8 +27,21 @@ public class Wrist extends SubsystemBase {
     private void stopMotor() {
         wristMotor.stopMotor();
     }
+    private boolean atBranch = (wristEncoder.get() == 180);
+    public Command placeCoral() {
+        return Commands.run(() -> setSpeed(10), this)
+                .until(() -> !atBranch)
+                .andThen(() -> rollers.outtakeCoralCommand());
+    }
+    private boolean atSource = (wristEncoder.get() == 360);
+    public Command getCoral() {
+        return Commands.run(() -> setSpeed(10), this)
+                .until(() -> !atSource)
+                .finallyDo(() -> rollers.intakeCoralCommand());
+    }
     public void periodic(){
         largeWristAngle = wristEncoder.get();
         smallWristAngle = largeWristAngle * (30/14);
+        
     }
 }
