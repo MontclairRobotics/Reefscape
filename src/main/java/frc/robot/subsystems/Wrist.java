58aec479cc7e705e0c2b115f9c093408018a800f
sub0dev <125705137+Mr-Pyro@users.noreply.github.com>
2025-02-07 19:15:59 -0500

    Update Wrist.java

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -2,46 +2,48 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import frc.robot.subsystems.Rollers;
 
 public class Wrist extends SubsystemBase {
+    public final double WRIST_SPEED = 0.5;
+    public final double MAX_VELOCITY = 0.5;
+    public final double MAX_ACCELERATION = 0.5;
+    private final ProfiledPIDController wristController;
+    private Encoder encoder;
     private SparkMax wristMotor;
     public static Rollers rollers = new Rollers();
     public double smallWristAngle;
     public double largeWristAngle;
-    Encoder wristEncoder = (Encoder) wristMotor.getEncoder();
     public Wrist() {
+        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(2.0, 1.0);
         wristMotor = new SparkMax(13, MotorType.kBrushless);
+        encoder = new Encoder(0,1);
+        wristController = new ProfiledPIDController(0.5, 0.0, 0.1, constraints);
+        encoder.setDistancePerPulse(1.0);
     }
 
-    private void setSpeed(double wristSpeed) {
-        wristMotor.set(wristSpeed);
-    }
-
-    private void stopMotor() {
+    public void stopMotor() {
         wristMotor.stopMotor();
     }
-    private boolean atBranch = (wristEncoder.get() == 180);
-    public Command placeCoral() {
-        return Commands.run(() -> setSpeed(10), this)
-                .until(() -> !atBranch)
-                .andThen(() -> rollers.outtakeCoralCommand());
+    public void setWristTarget(double targetPosition) {
+        wristController.setGoal(targetPosition);
     }
-    private boolean atSource = (wristEncoder.get() == 360);
-    public Command getCoral() {
-        return Commands.run(() -> setSpeed(10), this)
-                .until(() -> !atSource)
-                .finallyDo(() -> rollers.intakeCoralCommand());
+    public void updatePID() {
+        double currentPosition = encoder.getDistance();
+        double pidOutput = wristController.calculate(currentPosition);
+        wristMotor.set(pidOutput); 
     }
     public void periodic(){
-        largeWristAngle = wristEncoder.get();
-        smallWristAngle = largeWristAngle * (30/14);
+        largeWristAngle = encoder.getDistance();
+        smallWristAngle = (largeWristAngle * (30.0/14.0)) + 30;
+        updatePID();
         
     }
 }
