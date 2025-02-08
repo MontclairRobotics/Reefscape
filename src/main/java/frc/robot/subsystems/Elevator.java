4b59891f80abf5eba20539b2deeb0dc0d00cb65d
Abraham17-K <abrahamkirschner1@gmail.com>
2025-02-08 18:49:28 -0500

    Revert "Updated safeties"
    This reverts commit 6f2c4efb45810816318f5cf0ac143b5daca415aa.

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -72,8 +72,6 @@ public class Elevator extends SubsystemBase {
                                                      // thinks it is all the way at the top or bottom of the elevator
                                                      // and is trying to go farther but has not yet hit the limit switch
                                                      // during manual control
-    private static final double PERCENT_HEIGHT_DEADBAND = 0.004; //(As a decimal not percent) Ex. 0.04 is 4%
-                                                     //The deaband used in manual control for percent height                                                 
     public static final double MAX_VELOCITY_RPS = 100;
     public static final double MAX_ACCEL_RPS = 200;
 
@@ -345,7 +343,7 @@ public class Elevator extends SubsystemBase {
 
         double voltage = Math.pow(-MathUtil.applyDeadband(RobotContainer.operatorController.getLeftY(), 0.04), 3) * 12;
 
-        // voltage = accelerationLimiter.calculate(voltage); //TODO: uncomment
+        // voltage = accelerationLimiter.calculate(voltage);
 
         double percentHeight = this.getExtension() / MAX_EXTENSION;
         // System.out.println(voltage);
@@ -353,23 +351,37 @@ public class Elevator extends SubsystemBase {
         // System.out.println("Elevator ff Voltage: " + elevatorFeedforward.calculate(0));
 
         if (voltage < 0) {
-            if (percentHeight <= 0.04) {
+            if (percentHeight <= 0.004) {
+                voltage = 0;
                 accelerationLimiter.reset(0);
                 System.out.println("At bottom!---------------------");
+            } else if (percentHeight <= 0.07) {
+                System.out.println("Adding " + ((12 * Math.pow((percentHeight * (100.0 / SLOW_DOWN_ZONE)), 3.0)) - SLOWEST_SPEED));
+                voltage = Math.max(voltage, (-12 * Math.pow((percentHeight * (100.0 / SLOW_DOWN_ZONE)), 3.0)) - SLOWEST_SPEED);
             }
         }
-
-        voltage = MathUtil.clamp(voltage, -12 * (Math.pow( (MathUtil.applyDeadband(percentHeight, PERCENT_HEIGHT_DEADBAND) * (100.0 / SLOW_DOWN_ZONE)), 3.2)) - SLOWEST_SPEED, /*lowest voltage allowed*/
-         12 * (Math.pow( (MathUtil.applyDeadband(1 - percentHeight, PERCENT_HEIGHT_DEADBAND)) * (100.0 / SLOW_DOWN_ZONE), 3.2)) + SLOWEST_SPEED) /*highest voltage allowed*/;
-        //This clamps the voltage as it gets closer to the the top or the bottom. The slow down zone is the area at the top or the bottom when things.
-        //The slowest speed will allow the elevator to still go up and down no mater what as long it has not hit the limit switch 
-        //Puting it to the power of 3 (or somthing else) makes the slowdown more noticable
-        //The formula is 12 * (percentHeight * 100/7)^3
+        // if (voltage > 0) {
+        //     if (percentHeight >= 0.996) {
+        //         voltage = 0;
+        //         accelerationLimiter.reset(0);
+        //     } else if (percentHeight >= 0.93) {
+        //         voltage = Math.min(voltage, (12 * Math.pow((percentHeight * (100.0 / SLOW_DOWN_ZONE)), 3.0)) + SLOWEST_SPEED);
+        //     }
+        // }
 
         voltage = voltage + elevatorFeedforward.calculate(0);
 
         voltage = MathUtil.clamp(voltage, -12, 12);
         voltagePub.set(voltage);
+
+        // voltage = MathUtil.clamp(voltage, -(12 * Math.pow((percentHeight * (100.0 / SLOW_DOWN_ZONE)), 3.0)
+        //         - SLOWEST_SPEED), /* lowest voltage allowed */
+        //         (12 * ((1 - percentHeight) * (100.0 / SLOW_DOWN_ZONE))) + SLOWEST_SPEED) /* highest voltage allowed */;
+        // This clamps the voltage as it gets closer to the the top or the bottom. The
+        // slow down zone is the area at the top or the bottom when things.
+        // The slowest speed will allow the elevator to still go up and down no mater
+        // what as long it has not hit the limit switch
+        // Puting it to the power of 3 makes the slowdown more noticable
 
         // speed
         // if (isAtTop() && voltage > 0) {
