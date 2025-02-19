82d0bb3d81ff2d2a33a8b6e04613638e472939bb
sub0dev <125705137+Mr-Pyro@users.noreply.github.com>
2025-02-19 16:21:07 -0500

    Update Rollers.java

--- src/main/java/frc/robot/subsystems/Rollers.java
+++ src/main/java/frc/robot/subsystems/Rollers.java
@@ -3,12 +3,16 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.GamePiece;
 
 public class Rollers extends SubsystemBase {
+    private NetworkTableEntry entry;
     private SparkMax rightMotor;
     private SparkMax leftMotor;
     public final double CORAL_INTAKE_SPEED = 1;
@@ -22,6 +26,10 @@ public class Rollers extends SubsystemBase {
     public Rollers() {
         rightMotor = new SparkMax(13, MotorType.kBrushless);
         leftMotor = new SparkMax(14, MotorType.kBrushless);
+        NetworkTableInstance nt = NetworkTableInstance.getDefault();
+        nt.startServer(); 
+        entry = nt.getTable("Testing").getEntry("IsHeld");
+
     }
 
     public GamePiece getHeldPiece() {
@@ -92,5 +100,10 @@ public class Rollers extends SubsystemBase {
                     stopMotors();
                     this.heldPiece = GamePiece.None;
                 }).withTimeout(2); // TODO find timeout
+    }
+    @Override
+    public void periodic() {
+        boolean isHeld = (heldPiece != GamePiece.None)&&!(DriverStation.isAutonomousEnabled());
+        entry.setBoolean(isHeld);
     }
 }
