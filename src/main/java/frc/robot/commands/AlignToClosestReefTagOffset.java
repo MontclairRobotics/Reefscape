5fb878be3aabfc73e86eed30acea0c066d45d231
Steven Pearson <spearson23@gmail.com>
2025-03-18 12:31:46 -0400

    Clean up

--- src/main/java/frc/robot/commands/AlignToClosestReefTagOffset.java
+++ src/main/java/frc/robot/commands/AlignToClosestReefTagOffset.java
@@ -14,136 +14,101 @@ import frc.robot.vision.Limelight;
 import frc.robot.vision.LimelightHelpers;
 import frc.robot.vision.LimelightHelpers.RawFiducial;
 
-import org.littletonrobotics.junction.Logger;
-
-
 public class AlignToClosestReefTagOffset extends Command {
     
-//     private PIDController xController;
-//     private PIDController yController;
-//     private PIDController thetaController;
+    private PIDController xController;
+    private PIDController yController;
+    private PIDController thetaController;
 
-//     private Limelight camera;
-//     //private int tagID;
-//     private static double xOffset;
-//     private static double yOffset;
-//     private double thetaOffset;
+    private Limelight camera;
+    //private int tagID;
+    private static double xOffset;
+    private static double yOffset;
+    private double thetaOffset;
 
 
-//     public AlignToClosestReefTagOffset(TagOffset offset, boolean isOffset) {
-//         if (offset.isLeft()) {
-//             camera = RobotContainer.rightLimelight;
-//         } else {
-//             camera = RobotContainer.leftLimelight;
-//         }
+    public AlignToClosestReefTagOffset(TagOffset offset, boolean isOffset) {
+        if (offset.isLeft()) {
+            camera = RobotContainer.leftLimelight;
+        } else {
+            camera = RobotContainer.rightLimelight;
+        }
 
-//         xOffset = offset.getXOffsetM();
-//         if (isOffset) {
-//             yOffset = 0.3;
-//         } else {
-//             yOffset = 0.02;
-//         }
+        xOffset = offset.getXOffsetM();
+        if (isOffset) {
+            yOffset = 0.3;
+        } else {
+            yOffset = 0.02;
+        }
 
-//         //TODO: tune + make global PID Constants
-//         xController = new PIDController(3.5, 0, 0);
-//         xController.setTolerance(0.02); //0.5 degrees, I think? if its based on tx
-//         yController = new PIDController(3.5, 0, 0);
-//         yController.setTolerance(0.02); //degrees
-//         thetaController = RobotContainer.drivetrain.thetaController;
-//         addRequirements(RobotContainer.drivetrain);
-//     }
+        //TODO: tune + make global PID Constants
+        xController = new PIDController(3.5, 0, 0);
+        xController.setTolerance(0.02); //0.5 degrees, I think? if its based on tx
+        yController = new PIDController(3.5, 0, 0);
+        yController.setTolerance(0.02); //degrees
+        thetaController = RobotContainer.drivetrain.thetaController;
+        addRequirements(RobotContainer.drivetrain);
+    }
 
-//     @Override
-//     public void initialize(){
-//         //sets the tx and ty setpoints
-//         // TODO we want our setpoints to be zero, right?
+    @Override
+    public void initialize(){
+        //sets the tx and ty setpoints
+        // TODO we want our setpoints to be zero, right?
 
-//         RawFiducial closest = camera.getClosestTag();
-//         if (closest == null) {
-// <<<<<<< HEAD
-//             System.out.println("**************** CANCELED IN INIT ****************");
-//             cancel();
-//             return;
-//         }
-//         System.out.println("CLOSEST: " + closest.id);
-// =======
-//             cancel();
-//             return;
-//         } 
-// >>>>>>> rafael/new-auto
-//         thetaOffset = Limelight.tagRotationsMap.get(closest.id).getRadians();
+        RawFiducial closest = camera.getClosestTag();
+        if (closest == null) {
+            cancel();
+            return;
+        } 
+        thetaOffset = Limelight.tagRotationsMap.get(closest.id).getRadians();
 
-//         xController.setSetpoint(xOffset);
-//         yController.setSetpoint(yOffset);
-//         thetaController.setSetpoint(thetaOffset);
-//     }
+        xController.setSetpoint(xOffset);
+        yController.setSetpoint(yOffset);
+        thetaController.setSetpoint(thetaOffset);
+    }
 
-//     @Override
-//     public void execute() {
-//         //TODO: make these get the correct value
+    @Override
+    public void execute() {
+        //TODO: make these get the correct value
 
-//         //PID calculated outputs
-//         double xSpeed = 0;
-//         double ySpeed = 0;
+        //PID calculated outputs
+        double xSpeed = 0;
+        double ySpeed = 0;
 
-//         RawFiducial closest = camera.getClosestTag();
-//         if (closest == null) {
-// <<<<<<< HEAD
-//             System.out.println("**************** CANCELED ****************");
-//             cancel();
-//             return;
-//         }
+        RawFiducial closest = camera.getClosestTag();
+        if (closest == null) {
+            cancel();
+            return;
+        } 
+        //TODO will this just get stuck against a wall?
+        double thetaSpeed = thetaController.calculate(RobotContainer.drivetrain.getRobotPose().getRotation().getRadians());
 
-// =======
-//             cancel();
-//             return;
-//         } 
-// >>>>>>> rafael/new-auto
-//         //TODO will this just get stuck against a wall?
-//         double thetaSpeed = thetaController.calculate(RobotContainer.drivetrain.getRobotPose().getRotation().getRadians());
-//         Logger.recordOutput("AlignCommand/xDist", thetaSpeed);
+        // if (thetaController.atSetpoint()) {
 
-//         // if (thetaController.atSetpoint()) {
+            double xDist = closest.distToCamera * -Math.sin(closest.txnc * (Math.PI / 180.0)) + camera.cameraOffsetX; // TODO invert?
+            double yDist = closest.distToCamera * -Math.cos(closest.txnc * (Math.PI / 180.0)) + camera.cameraOffsetY;
 
-//             double xDist = closest.distToCamera * -Math.sin(closest.txnc * (Math.PI / 180.0)) + camera.cameraOffsetX; // TODO invert?
-//             double yDist = closest.distToCamera * -Math.cos(closest.txnc * (Math.PI / 180.0)) + camera.cameraOffsetY;
+            Logger.recordOutput("ClosestAlign/xDist", yDist);
+            Logger.recordOutput("ClosestAlign/yDist", xDist);
+            // TODO these may need inverts
+            xSpeed = xController.calculate(xDist);
+            ySpeed = yController.calculate(yDist);
+        // }
 
-// <<<<<<< HEAD
-//             Logger.recordOutput("AlignCommand/xDist", xDist);
-//             Logger.recordOutput("AlignCommand/yDist", yDist);
-//             System.out.println("xDist: " + xDist + " yDist: " + yDist);
+        //drives robot relative because tx and ty are robot relative
+        //no rotation input, we assume this is being used when robot is aligned heading-wise, but not translationally
+        //can add one to also move rotationally then translate later
+        //doesn't respect operator persective (this doesn't matter because its robot relative anyways)
+        RobotContainer.drivetrain.driveWithSetpoint(0, -xSpeed, thetaSpeed, false, false, false);
+    }
 
-// =======
-//             Logger.recordOutput("ClosestAlign/xDist", yDist);
-//             Logger.recordOutput("ClosestAlign/yDist", xDist);
-// >>>>>>> rafael/new-auto
-//             // TODO these may need inverts
-//             xSpeed = xController.calculate(xDist);
-//             ySpeed = yController.calculate(yDist);
+    @Override
+    public void end(boolean interrupted) {
+        RobotContainer.drivetrain.drive(0, 0, 0, true, false);
+    }
 
-//             Logger.recordOutput("AlignCommand/xSpeed", xSpeed);
-//             Logger.recordOutput("AlignCommand/ySpeed", ySpeed);
-//             System.out.println("xSpeed: " + xSpeed + " ySpeed: " + ySpeed);
-//             // }
-
-//         //drives robot relative because tx and ty are robot relative
-//         //no rotation input, we assume this is being used when robot is aligned heading-wise, but not translationally
-//         //can add one to also move rotationally then translate later
-//         //doesn't respect operator persective (this doesn't matter because its robot relative anyways)
-// <<<<<<< HEAD
-//         RobotContainer.drivetrain.driveWithSetpoint(0, xDist, thetaSpeed, false, false, false);
-// =======
-//         RobotContainer.drivetrain.driveWithSetpoint(0, -xSpeed, thetaSpeed, false, false, false);
-// >>>>>>> rafael/new-auto
-//     }
-
-//     @Override
-//     public void end(boolean interrupted) {
-//         RobotContainer.drivetrain.drive(0, 0, 0, true, false);
-//     }
-
-//     @Override
-//     public boolean isFinished() {
-//         return (xController.atSetpoint() && yController.atSetpoint() && thetaController.atSetpoint()) || !camera.hasValidTarget();
-//     }
+    @Override
+    public boolean isFinished() {
+        return (xController.atSetpoint() && yController.atSetpoint() && thetaController.atSetpoint()) || !camera.hasValidTarget();
+    }
 }
